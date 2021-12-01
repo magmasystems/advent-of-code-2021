bedb82a55e3d2533784067458a0da4eeef60eade
magmasystems <magmasystems@gmail.com>
2021-12-01 07:13:24 -0500

    Initial Commit

---
+++ Day1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode2021
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var input = File.ReadAllLines(args.Length > 0 ? args[0] : "Input.txt");
+
+            var numIncreasing = 0;
+            var lastValue = Int32.MinValue;
+            
+            foreach (var line in input)
+            {
+                int n = Convert.ToInt32(line);
+                if (lastValue != Int32.MinValue && n > lastValue)
+                    numIncreasing++;
+                lastValue = n;
+            }
+
+            Console.WriteLine($"The number of increasing values is {numIncreasing}");
+        }
+    }
+}
