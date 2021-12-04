e500b019cf74e2583d9c6f8187535990edb73636
magmasystems <magmasystems@gmail.com>
2021-12-04 14:06:18 -0500

    Some refactoring of the Bingo Board for speed

--- Day4/Board.cs
+++ Day4/Board.cs
@@ -3,21 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
+// ReSharper disable once CheckNamespace
 namespace AdventOfCode2021
 {
     public class Board
     {
+        private class BoardCell
+        {
+            public readonly int Row;
+            public readonly int Col;
+            public bool Marked;
+
+            public BoardCell(int row, int col)
+            {
+                Row = row;
+                Col = col;
+            }
+        }
+        
         private const int DIM = 5;
         private static int BOARD_ID;
 
         public int Id { get; }
-        private int[,] Matrix { get; } = new int[DIM, DIM];
+
+        // This is used to test membership of a number within the matrix. It holds the position of the number.
+        private readonly Dictionary<int, BoardCell> BoardCells = new();
 
         private List<BitArray> RowMatches { get; } = new(DIM);
         private List<BitArray> ColumnMatches { get; } = new(DIM);
         private BitArray LeftDiagonalMatches { get; } = new(DIM);  // 0,0 1,1 2,2 3,3 4,4
         private BitArray RightDiagonalMatches { get; } = new(DIM); // 0,4 1,3 2,2 3,1 4,0
-
+        
         public Board()
         {
             this.Id = BOARD_ID++;
@@ -31,36 +47,34 @@ namespace AdventOfCode2021
         public void AddRow(int idxRow, int[] nums)
         {
             for (var i = 0; i < nums.Length; i++)
-                this.Matrix[idxRow, i] = nums[i];
+            {
+                this.BoardCells.Add(nums[i], new BoardCell(idxRow, i));
+            }
         }
 
         public bool CallNumber(int number)
         {
-            for (var row = 0; row < DIM; row++)
+            if (!this.BoardCells.TryGetValue(number, out var cell))
+                return false;
+
+            var row = cell.Row;
+            var col = cell.Col;
+
+            cell.Marked = true;
+            this.RowMatches[row][col] = true;
+            this.ColumnMatches[col][row] = true;
+            
+            if (row == col)
             {
-                for (var col = 0; col < DIM; col++)
-                {
-                    if (number == this.Matrix[row, col])
-                    {
-                        this.RowMatches[row][col] = true;
-                        this.ColumnMatches[col][row] = true;
-                        
-                        if (row == col)
-                        {
-                            this.LeftDiagonalMatches[row] = true;
-                        }
-
-                        if (row + col == DIM - 1)
-                        {
-                            this.RightDiagonalMatches[col] = true;
-                        }
-
-                        return true;
-                    }
-                }
+                this.LeftDiagonalMatches[row] = true;
             }
 
-            return false;
+            if (row + col == DIM - 1)
+            {
+                this.RightDiagonalMatches[col] = true;
+            }
+
+            return true;
         }
 
         public bool IsBingo
@@ -89,17 +103,7 @@ namespace AdventOfCode2021
         {
             get
             {
-                var sum = 0;
-                for (var row = 0; row < DIM; row++)
-                {
-                    for (var col = 0; col < DIM; col++)
-                    {
-                        if (this.RowMatches[row][col] == false)
-                            sum += this.Matrix[row, col];
-                    }
-                }
-
-                return sum;
+                return this.BoardCells.Where(kvp => !kvp.Value.Marked).Sum(kvp => kvp.Key);
             }
         }
 
