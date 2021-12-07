ce0788b7e060e249d0bd05b77f274a9f059b887e
magmasystems <magmasystems@gmail.com>
2021-12-07 18:06:27 -0500

    Did a little refactoring for Day 7

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -11,53 +11,46 @@ namespace AdventOfCode2021
         private static void Main(string[] args)
         {
             var input = File.ReadAllLines(args.Length > 0 ? args[0] : "Input.txt");
-            var values = input[0].Split(',', StringSplitOptions.RemoveEmptyEntries).Select(s => Convert.ToInt32(s)).ToArray();
+            var positions = input[0].Split(',', StringSplitOptions.RemoveEmptyEntries).Select(s => Convert.ToInt32(s)).ToArray();
 
-            Dictionary<int, int> valueToCount = new();
-            foreach (var value in values)
+            // Create a dictionary of each position and how many crabs are at that position
+            Dictionary<int, int> numCrabsAtPositions = new();
+            foreach (var value in positions)
             {
-                if (valueToCount.ContainsKey(value))
-                    valueToCount[value]++;
+                if (numCrabsAtPositions.ContainsKey(value))
+                    numCrabsAtPositions[value]++;
                 else
-                    valueToCount[value] = 1;
+                    numCrabsAtPositions[value] = 1;
             }
-            var maxPos = valueToCount.Keys.Max();
-
-            // Part 1
-            var minFuel = ulong.MaxValue;
             
-            for (var origin = 0; origin <= maxPos; origin++)
+            var maxPosition = numCrabsAtPositions.Keys.Max();
+            var accumulators = new Func<int, int, int, ulong>[]
             {
-                ulong totalFuel = 0L;
-                foreach (var (key, count) in valueToCount)
-                {
-                    totalFuel += (ulong) Math.Abs((key - origin) * count);
-                }
+                (key, count, origin) => (ulong) Math.Abs((key - origin) * count),                            // Part 1
+                (key, count, origin) => (ulong) MathUtils.Factorial(Math.Abs(key - origin)) * (ulong) count  // Part 2
+            };
 
-                if (totalFuel < minFuel)
-                    minFuel = totalFuel;
-            }
-            
-            Console.WriteLine($"Part 1: The minimum fuel is {minFuel}");
-            
-            // Part 2
-            minFuel = ulong.MaxValue;
-
-            for (var origin = 0; origin <= maxPos; origin++)
+            for (var part = 1; part <= 2; part++)
             {
-                ulong totalFuel = 0L;
-                foreach (var (key, count) in valueToCount)
+                var minFuel = ulong.MaxValue;
+
+                // Go through all of the possible positions, from 0 until the maximum position.
+                // Accumulate the total fuel used from the origin until the crab position.
+                for (var origin = 0; origin <= maxPosition; origin++)
                 {
-                    totalFuel += (ulong) MathUtils.Factorial(Math.Abs(key - origin)) * (ulong) count;
+                    ulong totalFuel = 0L;
+                    foreach (var (key, count) in numCrabsAtPositions)
+                        totalFuel += accumulators[part - 1](key, count, origin);
+
+                    if (totalFuel < minFuel)
+                        minFuel = totalFuel;
                 }
 
-                if (totalFuel < minFuel)
-                {
-                    minFuel = totalFuel;
-                }
+                Console.WriteLine($"Part {part}: The minimum fuel is {minFuel}");
             }
-            
-            Console.WriteLine($"Part 2: The minimum fuel is {minFuel}");
+
+            // Part 1: The minimum fuel is 347509
+            // Part 2: The minimum fuel is 98257206
         }
     }
 }
